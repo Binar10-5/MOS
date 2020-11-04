1ed38b4ca268f59c7180841924de2e357d648ec9
Ronaldo <RonaldoDe>
2020-11-04 11:52:13 -0500

    Fix

--- app/Models/Cupon.php
+++ app/Models/Cupon.php
@@ -8,7 +8,7 @@ class Cupon extends Model
 {
     protected $table = 'cupons';
     protected $fillable = [
-        'name', 'description', 'code', 'uses_number', 'minimal_cost', 'discount_amount' ,'state', 'maximum uses'
+        'name', 'description', 'code', 'uses_number', 'minimal_cost', 'discount_amount' ,'state', 'maximum_uses'
     ];
 
     public function scopeName($query, $name)
