cb0651038095a7046242a63524505ff7328421a0
Ronaldo <RonaldoDe>
2020-11-25 16:30:42 -0500

    Valida payment State

--- app/Http/Controllers/Api/Administration/ExternalConnections/PayURequestController.php
+++ app/Http/Controllers/Api/Administration/ExternalConnections/PayURequestController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Api\Administration\ExternalConnections;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cupon;
 use App\Models\Error;
 use App\Models\Order;
+use App\Models\OrderProducts;
+use App\Models\ProductVariant;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PayURequestController extends Controller
 {
@@ -17,22 +22,66 @@ class PayURequestController extends Controller
         $order->payment_data = json_encode($request->all());
         $order->update();
         return response()->json(['response' => 'Success'], 200);
-        $validator=\Validator::make($request->all(),[
-            'payment_state' => 'required',
-            'description' => 'required|min:1',
-            'order_id' => 'required'
-        ]);
-        if($validator->fails())
-        {
-          return response()->json(['response' => ['error' => $validator->errors()->all()]],400);
+
+
+        $order = Order::where('order_number', request('reference_sale'))->first();
+
+        if(!$order){
+            Error::cretae([
+                'description'=> 'No se encontró la orden que manda el reference_sale de payU',
+                'type'=> 1
+            ]);
+            return response()->json(['response' => ['error' => ['Orden no existente']]], 400);
         }
 
-        $order = Order::find(request('order_id'));
+        DB::beginTransaction();
+        try{
+            if(request('response_message_pol') != 'APPROVED'){
+                $products = OrderProducts::where('order_id', $order->id)->get();
 
-        if(request('payment_state') != 3){
+                foreach ($products as $product) {
+                    $variant = ProductVariant::find($product->id);
 
-        }else{
+                    if(!$variant){
+                        Error::cretae([
+                            'description'=> 'No se encontró la variante de el producto en el momento de devolver el inventario, el id es el: '.$product->id,
+                            'type'=> 2
+                        ]);
+                        return response()->json(['response' => ['error' => ['Variante de producto no encontrada']]], 400);
+                    }
 
+                    $variant->quantity += $product->quantity;
+                    $variant->update();
+                }
+
+                if($order->coupon_id != null || $order->coupon_id != ''){
+                    $coupon = Cupon::find($order->coupon_id);
+                    $coupon->uses_number -= 1;
+                    $coupon->update();
+                }
+
+                $order->payment_data = json_encode($request->all());
+                $order->state_id = 2;
+                $order->update();
+            }else{
+                if($order->coupon_id != null || $order->coupon_id != ''){
+                    $coupon = Cupon::find($order->coupon_id);
+                    if($coupon->uses_number >= $coupon->maximum_uses){
+                        $coupon->state = 2;
+                        $coupon->update();
+                    }
+                }
+
+                $order->payment_data = json_encode($request->all());
+                $order->state_id = 3;
+                $order->update();
+            }
+
+        }catch(Exception $e){
+            DB::rollback();
         }
+
+        DB::commit();
+        return response()->json(['response' => 'Success'], 200);
     }
 }
---
+++ app/Models/Error.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Error extends Model
+{
+    protected $table = 'errors';
+    protected $fillable = [
+        'description', 'type'
+    ];
+}
