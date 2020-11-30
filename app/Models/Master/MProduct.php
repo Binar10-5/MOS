b5e5f9d726c79cddf23d880336b158e7053a1098
Ronaldo <RonaldoDe>
2020-11-30 15:56:18 -0500

    Final price by product

--- app/Models/Master/MProduct.php
+++ app/Models/Master/MProduct.php
@@ -25,6 +25,34 @@ class MProduct extends Model
         }
     }
 
+    public function scopeCategory1($query, $category1)
+    {
+        if(!empty($category1)){
+            $query->where('c1.category1_id', $category1);
+        }
+    }
+
+    public function scopeCategory2($query, $category2)
+    {
+        if(!empty($category2)){
+            $query->where('c2.category2_id', $category2);
+        }
+    }
+
+    public function scopeCategory3($query, $category3)
+    {
+        if(!empty($category3)){
+            $query->where('c3.category3_id', $category3);
+        }
+    }
+
+    public function scopeBrand($query, $brand)
+    {
+        if(!empty($brand)){
+            $query->where('m_products.brand_id', $brand);
+        }
+    }
+
     public function scopeLanguageC1($query, $language)
     {
         if(!empty($language)){
