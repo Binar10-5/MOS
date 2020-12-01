5d41808cd89b1b13b01817a8f882a86ca3174386
Ronaldo <RonaldoDe>
2020-12-01 09:55:03 -0500

    Final price by product

--- resources/views/pqrs_client.blade.php
+++ resources/views/pqrs_client.blade.php
@@ -8,12 +8,15 @@
 </head>
 <body>
     <H2>Saludos, {{$data['name']}}</H2><br>
+    <h3>A continuación la PQR que nos enviastes.</h3>
     <p>
         Correo: {{$data['email']}}<br>
         pqrs: {{$data['pqrs']}}<br>
         Mensaje: {{$data['message']}}<br>
         Número de pqrs: {{$data['pqrs_id']}}<br>
     </p>
+
+    <H3>Atentamente, equipo MOS.</H3>
 </body>
 </html>
 
